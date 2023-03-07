eb74c8d54c7674f1c1affdeaa8a09f1830d6777a
Christian Axel Serrano Sandoval ( Chiles ) <61697287+Chiles012@users.noreply.github.com>
2023-03-06 20:56:33 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const bar = document.getElementById('bar');
+const line = document.getElementById('line');
+const pie = document.getElementById('pie');
+const doughnut = document.getElementById('doughnut');
+
